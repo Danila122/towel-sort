2c993596c360572b1e635688d784d42ea8d5cc11
Danila Olesyuk <olesyukdanila122@gmail.com>
2021-03-13 18:44:03 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,28 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+	if(matrix == undefined ){
+		return []
+	}
+	
+	let newArray = []
+
+	for (let i = 0; i < matrix.length; i++) {
+		if( i % 2 > 0 && i > 0){
+			matrix[i].reverse()
+		}
+		
+	}
+
+	for (let i = 0; i < matrix.length; i++) {
+		for (let j = 0; j < matrix[i].length; j++) {
+			newArray.push(matrix[i][j])
+			
+		}
+	}
+
+	matrix = newArray
+	return matrix;
+
+
 }
